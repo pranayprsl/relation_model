2306f2c7066a753b164becb9c3fb3f9abce75719
pranay <pranaylodeprsl1996@gmail.com>
2022-06-04 15:27:46 +0530

    hasmany and belongsTo

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -1,16 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\City;
-use App\Models\Village;
+use App\Models\Author;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function index($id)
     {
-        $city = City::find($id);
-        var_dump($city->village->village_name);
-
+      $author = Author::find($id);
+    //   var_dump($author->author_name);
+    // dd($author->post);
+    foreach($author->post as $post)
+     {
+         echo $post->title ;
+         echo "<br>";
+         echo $post->body;
+     }   
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,11 @@ Route::get('/add_city',[CityController::class,'add_city'])->name('add_city');
 Route::get('/show/{id}',[CityController::class,'show'])->name('show');
 Route::get('/showvillage/{id}',[VillageController::class,'showvillage'])->name('showvillage');
 
-Route::get('/index/{id}',[IndexController::class,'index'])->name('index');
+
 
 Route::get('/author',[AuthorController::class,'add_author'])->name('add_author');
 Route::get('/post/{id}',[PostController::class,'add_post'])->name('add_post');
 Route::get('/show/{id}',[PostController::class,'show'])->name('show');
 Route::get('/showauthor/{id}',[AuthorController::class,'showauthor'])->name('showauthor');
+
+Route::get('/index/{id}',[IndexController::class,'index'])->name('index');
